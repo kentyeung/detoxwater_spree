f271487338bea126d77b53ef95be6237758bb4a8
Kent Yeung <kent.yeung2@gmail.com>
2014-11-22 11:51:46 -0500

    First commit
    For designer to see checkout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DetoxwaterSpree::Application.config.secret_key_base = '5934bdf455740be535c6a9dae84eee1adc52062c050a77d1c378c56997cb16b19c00f692fbb47b61682a4c34b4cc87dfeb8ea9a00a3500355b6b422aff5b5c93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DetoxwaterSpree::Application.config.session_store :cookie_store, key: '_detoxwater_spree_session'
